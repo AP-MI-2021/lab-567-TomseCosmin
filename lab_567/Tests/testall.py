f64e5eb1908cc1999be0915c69f691d74353d965
TomseCosmin <91584937+TomseCosmin@users.noreply.github.com>
2021-11-10 08:37:45 +0200

    Update testall.py

--- lab_567/Tests/testall.py
+++ lab_567/Tests/testall.py
@@ -1,10 +1,10 @@
 from lab_567.Tests.testCRUD import test_adauga_obiect, test_sterge_obiect
 from lab_567.Tests.testDomeniu import test_obiect
-
+from lab_567.Tests.test_undo_redo import undo_redo_test
 
 
 def run_all_tests():
     test_obiect()
     test_adauga_obiect()
     test_sterge_obiect()
-
+    undo_redo_test()
